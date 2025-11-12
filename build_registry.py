100b3c6e655282fa668f86232f80abab0349fa0d
Ben Sparks <sparksbenjamin@gmail.com>
2025-11-12 09:00:44 -0500

    Create build_registry.py

---
+++ build_registry.py
@@ -0,0 +1,12 @@
+{
+  "workspaces": [
+    {
+      "name": "Obsidian",
+      "id": "obsidian",
+      "description": "Obsidian Markdown Workspace",
+      "image": "ghcr.io/YOUR_GITHUB_USERNAME/obsidian:1.17.0",
+      "json_url": "https://raw.githubusercontent.com/sparksbenjamin/kasm_workspaces/refs/heads/main/workspaces/obsidian.json",
+      "icon": "https://raw.githubusercontent.com/YOUR_GITHUB_USERNAME/kasm-registry/main/icons/obsidian.png"
+    }
+  ]
+}
